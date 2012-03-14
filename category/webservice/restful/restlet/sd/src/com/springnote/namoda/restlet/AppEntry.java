53b1a4b405568150f6c7eb01bccda7d243798796
NHKimNamo@gmail.com <NHKimNamo@gmail.com@d89b2fd2-161f-832f-ac5e-88f7c91a39bd>
2012-03-14 02:13:25 +0000

    [webservice:restlet] Default Resource 추가(StringRepresentation) Default Resource <= Not Found
    The server has not found anything matching the request URI
    
    You can get technical details here.
    Please continue your visit at our home page.

--- category/webservice/restful/restlet/sd/src/com/springnote/namoda/restlet/AppEntry.java
+++ category/webservice/restful/restlet/sd/src/com/springnote/namoda/restlet/AppEntry.java
@@ -3,6 +3,7 @@ package com.springnote.namoda.restlet;
 import org.restlet.Application;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
+import com.springnote.namoda.restlet.resource.*;
 
 public class AppEntry extends Application {
 	/**
@@ -12,6 +13,9 @@ public class AppEntry extends Application {
 	public synchronized Restlet createInboundRoot() {
 		// Create a router Restlet that routes each call to a new instance of HelloWorldResource.
 		Router router = new Router(getContext());
+		
+		// Default route
+		router.attachDefault(Default.class);
 
 		return router;		
 	}
