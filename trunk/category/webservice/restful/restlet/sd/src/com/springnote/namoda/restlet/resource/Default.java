7c51cdf3308d635ab4964cb01fa5141aa707c7ae
NHKimNamo@gmail.com <NHKimNamo@gmail.com@d89b2fd2-161f-832f-ac5e-88f7c91a39bd>
2012-03-14 02:22:00 +0000

    [webservice:restlet] Default Resource 정적 HTML파일에서 읽어옴

--- trunk/category/webservice/restful/restlet/sd/src/com/springnote/namoda/restlet/resource/Default.java
+++ trunk/category/webservice/restful/restlet/sd/src/com/springnote/namoda/restlet/resource/Default.java
@@ -1,13 +1,17 @@
 package com.springnote.namoda.restlet.resource;
 
+import org.restlet.data.MediaType;
+import org.restlet.representation.FileRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
 public class Default extends ServerResource {
 	@Get("html")
 	public Representation representation() {
-		return new StringRepresentation("Default Resource");
+		FileRepresentation f = new FileRepresentation(
+				this.getClass().getResource("/com/springnote/namoda/restlet/resource/html/index.html").getPath(),
+				MediaType.TEXT_HTML);
+		return f;
 	}
 }
